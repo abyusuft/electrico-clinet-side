d9281a4f365483538927223b1b5bb1ccd508a284
abyusuft <imabuyusuf@gmail.com>
2022-05-26 23:15:27 +0600

    Components/pages added

---
+++ src/components/Pages/Home/Business.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Business = () => {
+    return (
+        <div>
+            <h2>This is Business Section</h2>
+        </div>
+    );
+};
+
+export default Business;
---
+++ src/components/Pages/Home/Reviews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reviews = () => {
+    return (
+        <div>
+            <h2>This is Review Section</h2>
+        </div>
+    );
+};
+
+export default Reviews;
