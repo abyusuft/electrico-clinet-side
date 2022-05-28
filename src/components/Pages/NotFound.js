fe6907e6e73e87073d45768c30a083ce0de8edf3
abyusuft <imabuyusuf@gmail.com>
2022-05-28 15:33:57 +0600

    404 page updated

--- src/components/Pages/NotFound.js
+++ src/components/Pages/NotFound.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import notFound from '../../images/dddddd.png'
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>This is Not Found Page</h2>
+        <div class="card  bg-[#F7F7F7] shadow-xl">
+            <figure class="px-10 pt-10">
+                <img src={notFound} alt="Shoes" />
+            </figure>
+            <div class="card-body items-center text-center">
+                <div class="card-actions">
+                    <Link to='/' class="btn btn-error btn-xl text-white">Back to Homepage</Link>
+                </div>
+            </div>
         </div>
     );
 };
